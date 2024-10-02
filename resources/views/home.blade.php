2e6cc4d2fdb62091eb0bc184d08a2ffeaf2020fb
fabiomarzano <fabiomarzano1993@gmail.com>
2024-10-03 01:06:11 +0200

    [UPD] add-home-blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('page-title') Comics Home  @endsection
+
+@section('content') 
+
+    <main>
+        <div class="container">
+            <h1>Sono nella Home</h1>
+        </div>
+    </main>
+
+@endsection
